e0490d45196b0c4f520c8012216f3b5883ed6e64
youwol-guillaume <reinisch.gui@gmail.com>
2025-09-04 16:49:55 +0200

    👽️ [tdse-1d] sync. w/ chapter API

--- src/chapters/tdse1d/src/lib/index.ts
+++ src/chapters/tdse1d/src/lib/index.ts
@@ -26,7 +26,13 @@ export async function installCodeApiModule() {
 const eq = String.raw`\(i \hbar \frac{\partial}{\partial t} \left| \Psi(t) \right\rangle 
 = \hat{H}(t) \left| \Psi(t) \right\rangle \)`
 
-export const navigation = async ({ context }: { context: ContextTrait }) => {
+export const navigation = async ({
+    context,
+    mountPath,
+}: {
+    context: ContextTrait
+    mountPath: string
+}) => {
     return {
         name: 'Schrödinger 1D',
         header: {
@@ -52,7 +58,7 @@ export const navigation = async ({ context }: { context: ContextTrait }) => {
                             entryModule: 'w3gallery_tdse1d',
                             dataFolder: `${getUrlBase(setup.name, setup.version)}/assets/api/tdse-1d`,
                             rootModulesNav: {
-                                w3gallery_tdse1d: '@nav/tdse-1d/backend/api',
+                                w3gallery_tdse1d: `@nav${mountPath}/backend/api`,
                             },
                             configuration: codeApiModule.configurationPython,
                         }),
@@ -74,3 +80,18 @@ export const navigation = async ({ context }: { context: ContextTrait }) => {
 }
 
 export const links = LinksDict
+
+export const title =
+    'Time-Dependent Schrödinger Equation in One Dimension (TDSE-1D)'
+export const abstract = `
+This chapter introduces the numerical study of the one-dimensional Time-Dependent Schrödinger Equation. 
+The first part demonstrates how **Pyodide** can be integrated within **mkdocs-ts** to present the theoretical background 
+and implement numerical recipes directly in the browser. The second part illustrates how **W3Nest** can be extended 
+with a **custom backend service**, used here to compute eigenstates and simulate time evolution. 
+
+Several canonical quantum scenarios are explored—including the harmonic oscillator, a Gaussian potential well, 
+and tunneling through a double well—showing how eigenstates, spectra, and wave-packet dynamics reveal both classical 
+analogies and uniquely quantum phenomena. 
+Interactive visualization tools (D3.js and Chart.js) complement the simulations, enabling readers to directly 
+connect numerical results with physical insights.
+`
